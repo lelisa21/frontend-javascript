837fdb5580d28c4dcbd52693e4d543ba0bc4f8aa
Lelisa Hailu <189599041+lelisa21@users.noreply.github.com>
2025-09-13 10:30:57 +0300

    Updated main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,29 +1,28 @@
-interface MajorCredits{
-    credits:number;
+// Add export to both interfaces and functions
+export interface MajorCredits {
+    credits: number;
     _brand: "major";
 }
-interface MinorCredits{
-    credits:number;
+
+export interface MinorCredits {
+    credits: number;
     _brand: "minor";
 }
 
-
-function sumMajorCredits (subject1: MajorCredits ,subject2: MajorCredits): MajorCredits{
-  return {
-    credits: subject1.credits + subject2.credits,
-    _brand: "major"
-  }
-}
-function sumMinorCredits (subject1: MinorCredits ,subject2: MinorCredits): MinorCredits{
-  return {
-    credits: subject1.credits + subject2.credits,
-    _brand: "minor"
-  }
+export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+    return {
+        credits: subject1.credits + subject2.credits,
+        _brand: "major"
+    };
 }
 
+export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+    return {
+        credits: subject1.credits + subject2.credits,
+        _brand: "minor"
+    };
+}
 
+// Test the functions
 console.log(sumMajorCredits({ credits: 13400, _brand: "major" }, { credits: 446, _brand: "major" }));
-//  { credits: 13846, _brand: 'major' }
-console.log(sumMinorCredits({ credits: 334, _brand: "minor" }, { credits: 456, _brand: "minor" }))
-//  { credits: 790, _brand: 'minor' }
-
+console.log(sumMinorCredits({ credits: 334, _brand: "minor" }, { credits: 456, _brand: "minor" }));
