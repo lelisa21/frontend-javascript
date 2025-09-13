155b8f1262f1db8a072bf4bf6447d110921e701a
Lelisa Hailu <189599041+lelisa21@users.noreply.github.com>
2025-09-13 10:33:58 +0300

    updated main.ts

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,45 +1,35 @@
+/// <reference path="subjects/Teacher.ts" />
+/// <reference path="subjects/Subject.ts" />
 /// <reference path="subjects/Cpp.ts" />
 /// <reference path="subjects/Java.ts" />
 /// <reference path="subjects/React.ts" />
 
-export const cpp = new Subjects.Cpp();
-export const java = new Subjects.Java();
-export const react = new Subjects.React();
+namespace Subjects {
+    export const cTeacher: Teacher = {
+        firstName: "Abeba",
+        lastName: "Tola",
+        experienceTeachingC: 10
+    };
 
-export const cTeacher: Subjects.Teacher = {
-  firstName: "Abeba",
-  lastName: "Tola",
-  experienceTeachingC: 10
-};
+    export const cpp = new Cpp();
+    export const java = new Java();
+    export const react = new React();
 
-console.log("C++");
-cpp.setTeacher(cTeacher);
-console.log(cpp.getRequirements());
-console.log(cpp.getAvailableTeacher());
-console.log("");
+    cpp.setTeacher(cTeacher);
+    java.setTeacher(cTeacher);
+    react.setTeacher(cTeacher);
 
-console.log("Java");
-java.setTeacher(cTeacher);
-console.log(java.getRequirements());
-console.log(java.getAvailableTeacher());
-console.log("");
+    console.log("C++:");
+    console.log(cpp.getRequirements());
+    console.log(cpp.getAvailableTeacher());
+    console.log();
 
-console.log("React");
-react.setTeacher(cTeacher);
-console.log(react.getRequirements());
-console.log(react.getAvailableTeacher());
+    console.log("Java:");
+    console.log(java.getRequirements());
+    console.log(java.getAvailableTeacher());
+    console.log();
 
-
-             /* outputs i got 
-C++
-Here is the list of requirements for Cpp
-Available Teacher: Abeba
-
-Java
-Here is the list of requirements for Java
-No available teacher
-
-React
-Here is the list of requirements for React
-No available teacher */
-
+    console.log("React:");
+    console.log(react.getRequirements());
+    console.log(react.getAvailableTeacher());
+}
