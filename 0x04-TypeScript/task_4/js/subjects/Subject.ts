e3e906de457fb82985d9bea966f580ad403c2a88
Lelisa Hailu <189599041+lelisa21@users.noreply.github.com>
2025-09-13 10:09:52 +0300

    Subject.ts updated

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,10 +1,6 @@
+/// <reference path="Teacher.ts" />
+
 namespace Subjects {
-
-    export interface Teacher {
-        firstName: string;
-        lastName: string;
-    }
-
     export interface Subject {
         setTeacher(teacher: Teacher): void;
     }
